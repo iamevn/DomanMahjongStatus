3c5cd28f3b6c14c0d329acebfd724df6682c7206
iamevn <iamevn@users.noreply.github.com>
2022-07-06 15:07:43 -0700

    Trim code to just try to read stats from UIState

--- DomanMahjongStatus/Plugin.cs
+++ DomanMahjongStatus/Plugin.cs
@@ -1,77 +1,27 @@
-using Dalamud;
-using Dalamud.Configuration;
-using Dalamud.Data;
-using Dalamud.Game.ClientState;
-using Dalamud.Game;
 using Dalamud.Game.Command;
 using Dalamud.Game.Gui;
-using Dalamud.IoC;
-using Dalamud.Logging;
 using Dalamud.Plugin;
-using FFXIVClientStructs.FFXIV.Client.UI;
-using FFXIVClientStructs.FFXIV.Component.GUI;
 using System;
-using System.Threading.Tasks;
-using Optional;
-using Optional.Linq;
-using Optional.Collections;
-using Optional.Unsafe;
 
 namespace DomanMahjongStatus
 {
     public class Plugin : IDalamudPlugin
     {
         public string Name => "Doman Mahjong Status";
+        private const string logCommandName = "/mjstat";
 
-        private const string commandName = "/majstat";
-        private const string dumpCommandName = "/dumpUIState";
-        private const string logCommandName = "/majlog";
-        private IntPtr addonPtr = IntPtr.Zero;
-
-        private IntPtr AddonPtr
-        {
-            get
-            {
-                var firstChoice = GameGui.GetAddonByName("EmjL", 1);
-                if (firstChoice == IntPtr.Zero)
-                    return GameGui.GetAddonByName("Emj", 1);
-                else
-                    return firstChoice;
-            }
-        }
-
-#pragma warning disable IDE0052
-        private DalamudPluginInterface PluginInterface { get; init; }
         private ChatGui ChatGui { get; init; }
-        private GameGui GameGui { get; init; }
-        private Framework Framework { get; init; }
         private CommandManager CommandManager { get; init; }
-#pragma warning restore IDE0052
 
-        public Plugin(DalamudPluginInterface pluginInterface, Framework framework, CommandManager commandManager, ChatGui chatGui, GameGui gameGui)
+        public Plugin(CommandManager commandManager, ChatGui chatGui)
         {
-            this.PluginInterface = pluginInterface;
-            this.Framework = framework;
             this.CommandManager = commandManager;
             this.ChatGui = chatGui;
-            this.GameGui = gameGui;
 
-            _ = CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
-            {
-                HelpMessage = "Write current mahjong status to chat log"
-            });
-
-            _ = CommandManager.AddHandler(dumpCommandName, new CommandInfo(OnDumpCommand)
-            {
-                HelpMessage = "Dump UIState bytes",
-            });
-
-            _ = CommandManager.AddHandler(logCommandName, new CommandInfo(OnLogCommand)
+            _ = CommandManager.AddHandler(logCommandName, new CommandInfo(OnStatCommand)
             {
                 HelpMessage = "Log mahjong stats parsed from UIState",
             });
-
-            // Framework.Update += PollMahjong;
         }
 
         public void Dispose()
@@ -84,86 +34,27 @@ namespace DomanMahjongStatus
         {
             if (disposing)
             {
-                Framework.Update -= PollMahjong;
-                _ = CommandManager.RemoveHandler(commandName);
-                _ = CommandManager.RemoveHandler(dumpCommandName);
                 _ = CommandManager.RemoveHandler(logCommandName);
             }
         }
 
-        private void OnDumpCommand(string command, string args)
-        {
-            ChatGui.Print("Dumping UIState...");
-            string dest = DebugUIState.Dump();
-            ChatGui.Print($"Dumped to {dest}");
-        }
-
-        private void OnLogCommand(string command, string args)
+        private void OnStatCommand(string command, string args)
         {
             ChatGui.Print("Reading UIState...");
-            DebugUIState.LogChat(ChatGui);
-        }
 
-        private void OnCommand(string command, string args)
-        {
-            PluginLog.Log("hello again from {Name}", Name);
-            if (AddonPtr != IntPtr.Zero)
+            string bytes = "[" + BitConverter.ToString(Stats.RankInfoBytes).Replace("-", ", ") + "]";
+            ChatGui.Print($"read {bytes} from UIState+0x{Stats.RankInfoOffset:X}");
+
+            if (Stats.Initialized)
             {
-                PluginLog.Log("mahjong addon at: {addonPtr}", AddonPtr.ToString("X"));
-                ChatGui.Print(" yup, looks like you're playing mahjong!");
-                try
-                {
-                    var reader = new UIReaderMahjongGame(AddonPtr);
-                    MahjongStatus gameState = reader.ReadMahjongStatus();
-                    ChatGui.Print(gameState.ToString());
-
-                    reader.ReadCurrentPlayer()
-                        .MatchSome(seat => ChatGui.Print($" current turn: {seat}"));
-
-                    if (reader.ScoreScreenFinished())
-                    {
-                        PluginLog.Log("score screen visible");
-                        Option<string> name = reader.GetWinnerName();
-                        Option<string> winType = reader.GetWinType();
-
-                        Option<string> hanFuText = reader.GetHanFuText();
-                        Option<string> score = reader.GetHandScoreText();
-                        var yakuList = reader.GetWinningYakuList();
-                        var yakuText = yakuList.Map(tup =>
-                        {
-                            (string yaku, string value) = tup;
-                            return $"{yaku} ({value})";
-                        });
-                        name.MatchSome(name => winType.MatchSome(winType => hanFuText.MatchSome(hanFu => score.MatchSome(score =>
-                            ChatGui.Print($"{name} {winType} with [{string.Join(", ", yakuText)}] for {hanFu} totaling {score}")))));
-                    }
-                }
-                catch (UIReaderMahjongGame.UIReaderError err)
-                {
-                    ChatGui.PrintError($"error parsing mahjong ui: {err.Message}");
-                }
+                ChatGui.Print($"Matches Played: {Stats.MatchCount}");
+                ChatGui.Print($"Current Rating: {Stats.CurrentRating}");
+                ChatGui.Print($"Highest Rating: {Stats.MaxRating}");
+                ChatGui.Print($"Rank: ??? (0x{Stats.Unknown1:X2} 0x{Stats.Unknown2:X2}) - {Stats.RankPoints} points");
             }
             else
             {
-                ChatGui.Print(" why aren't you playing mahjong?");
-            }
-            PluginLog.Log("end of command");
-        }
-
-        private void PollMahjong(Framework framework)
-        {
-            try
-            {
-                var maybeAddon = GameGui.GetAddonByName("EmjL", 1);
-                if (addonPtr == IntPtr.Zero)
-                    return;
-                addonPtr = maybeAddon;
-            }
-            catch (OperationCanceledException) { }
-            catch (Exception ex)
-            {
-                PluginLog.Error(ex, "Updater has crashed for {Name}", Name);
-                ChatGui.PrintError($"{Name} has encountered a critical error");
+                ChatGui.Print("Couldn't find stats, have you opened the Gold Saucer panel and have you played any mahjong?");
             }
         }
     }
---
+++ DomanMahjongStatus/Stats.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+
+namespace DomanMahjongStatus
+{
+    static class Stats
+    {
+        public static IntPtr UIStatePtr
+        {
+            get
+            {
+                unsafe
+                {
+                    return (IntPtr)UIState.Instance();
+                }
+            }
+        }
+
+        public const int UIStateSize = 0x168D8;
+        public const int RankInfoOffset = 0x14F78;
+        public const int RankInfoSize = 10;
+        public static IntPtr RankInfoPtr => UIStatePtr + RankInfoOffset;
+
+        public static short MatchCount => Marshal.ReadInt16(RankInfoPtr, 0);
+        public static short CurrentRating => Marshal.ReadInt16(RankInfoPtr, 2);
+        public static short MaxRating => Marshal.ReadInt16(RankInfoPtr, 4);
+        public static short RankPoints => Marshal.ReadInt16(RankInfoPtr, 6);
+        public static byte Unknown1 => Marshal.ReadByte(RankInfoPtr, 8);
+        public static byte Unknown2 => Marshal.ReadByte(RankInfoPtr, 9);
+
+        public static byte[] RankInfoBytes
+        {
+            get
+            {
+                byte[] barr = new byte[RankInfoSize];
+                Marshal.Copy(RankInfoPtr, barr, 0, RankInfoSize);
+
+                return barr;
+            }
+        }
+
+        public static bool Initialized
+        {
+            get
+            {
+                int sum = 0;
+                foreach (byte b in RankInfoBytes)
+                    sum += b;
+                return sum > 0;
+            }
+        }
+    }
+}
